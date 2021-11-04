2fc64d26ac57836d64cf1e4eabd5323c8a937b4d
Chocoloco123 <csarkki@gmail.com>
2021-11-04 09:24:33 -0700

    tested API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken()); // XSRF-TOKEN is a type of cookie that helps provide CSRF protection for your app
