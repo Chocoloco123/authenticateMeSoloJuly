32a874570afd0f3e3309ce422477fb9be8eccbbd
Chocoloco123 <csarkki@gmail.com>
2021-11-04 15:24:26 -0700

    commented out test route for GET /hello/world

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,10 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken()); // XSRF-TOKEN is a type of cookie that helps provide CSRF protection for your app
-  res.send('Hello World!');
-});
+// test route - safe to remove
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken()); // XSRF-TOKEN is a type of cookie that helps provide CSRF protection for your app
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
