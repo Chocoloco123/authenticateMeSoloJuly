0aea42cb4607b91bda8f837f32f40fbcae888f7b
Chocoloco123 <csarkki@gmail.com>
2021-11-05 16:45:54 -0700

    added package json

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,8 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-
-// test route - safe to remove
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken()); // XSRF-TOKEN is a type of cookie that helps provide CSRF protection for your app
-//   res.send('Hello World!');
-// });
 
 // Static routes
 // Serve React build files in production
@@ -18,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -29,17 +22,17 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
-// Add XSRF-TOKEN cookie for development
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
