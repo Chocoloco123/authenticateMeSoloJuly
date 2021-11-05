7dd1c9ea85b6b27f977ed4d3c0b23d12ed7a8010
Chocoloco123 <csarkki@gmail.com>
2021-11-04 21:45:20 -0700

    edited restore session user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import * as sessionActions from './store/session';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  
   useEffect(() => {
     dispatch(sessionActions.restoreUser())
       .then(() => setIsLoaded(true));
-  }, [dispatch])
+  }, [dispatch]);
 
   return isLoaded && (
     <Switch>
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -34,10 +34,11 @@ export const login = (user) => async (dispatch) => {
 
 export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch('/api/session');
-  const userData = await response.json();
-  dispatch(setUser(userData));
+  const data = await response.json();
+  console.log(data);
+  dispatch(setUser(data.user));
   return response;
-}
+};
 
 const initialState = { user: null };
 
