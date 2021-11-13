4510729a09755d7596e34d2a00189ec9156e0bad
Chocoloco123 <csarkki@gmail.com>
2021-11-12 23:09:50 -0800

    restyling navbar and created new logo

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-import IdyllicLogo from '../../media/IdyllicLogos/IdyllicLogo.png'
+import IdyllicLogo from '../../media/idyllic-new/idyllicLogoNewLargeBlack.png'
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -15,25 +15,42 @@ function Navigation({ isLoaded }){
     );
   } else {
     sessionLinks = (
-      <>
+      <div className='loginDiv'>
         <NavLink to="/login" className="navbarItem">Log In</NavLink>
-        {/* <NavLink to="/signup" className="navbarItem">Sign Up</NavLink> */}
-      </>
+      </div>
+
+      // <>
+      //   <NavLink to="/login" className="navbarItem">Log In</NavLink>
+      // </>
     );
   }
 
   return (
     <div className='boxAroundNav'>
-      <div className='navDiv'>
+      <a href='/' className='logoLink'>
         <img src={IdyllicLogo} className="loginLogo" alt="Idyllic logo"/>
-        <ul className='navUl'>
-          <li>
+      </a>
+      <div className='navDiv'>
+        {/* <ul className='navUl'> */}
+          {/* <li> */}
             <NavLink exact to="/home" className="navbarItem">Home</NavLink>
             {isLoaded && sessionLinks}
-          </li>
-        </ul>
+          {/* </li> */}
+        {/* </ul> */}
       </div>
     </div>
+
+    // <div className='boxAroundNav'>
+    //   <div className='navDiv'>
+    //     <img src={IdyllicLogo} className="loginLogo" alt="Idyllic logo"/>
+    //     <ul className='navUl'>
+    //       <li>
+    //         <NavLink exact to="/home" className="navbarItem">Home</NavLink>
+    //         {isLoaded && sessionLinks}
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
   );
 }
 
