984e9b2a417451758656bd635ff04a7c1e45fbd8
Chocoloco123 <csarkki@gmail.com>
2022-01-24 19:58:11 -0800

    successfully got backend connected properly

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,12 +6,14 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const imagesRouter = require('./images.js');
 const commentsRouter = require('./comments.js');
+const albumsRouter = require('./albums.js')
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/images', imagesRouter);
 // router.use('/comments', commentsRouter);
 router.use('/comments', commentsRouter);
+router.use('/albums', albumsRouter);
 
 // can now safely remove this test route
 // router.post('/test', (req, res) => {
