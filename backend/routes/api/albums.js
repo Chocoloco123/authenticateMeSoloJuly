589b6adf13e452aaf9ac0e630879307deb08664d
Chocoloco123 <csarkki@gmail.com>
2022-01-24 16:16:16 -0800

    added albums branch and started backend GET route

---
+++ backend/routes/api/albums.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+
+const { Album } = require('../../db/models');
+const { check, validationResult } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { Op } = require('sequelize')
+
+const router = express.Router();
+
+router.get('/albums', asyncHandler(async(req, res) => {
+  const albums = await Album.findAll();
+  res.json(albums);
+}))
+
+module.exports = router;
