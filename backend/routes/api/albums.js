fd03eeccadbc84ea8baf0d71beb60b1c6239ba5e
Chocoloco123 <csarkki@gmail.com>
2022-01-25 10:17:47 -0800

    added update backend route for albums

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -15,13 +15,52 @@ router.get('/', asyncHandler(async(req, res) => {
   res.json(albums);
 }))
 
-router.post('/newAlbum', requireAuth,asyncHandler(async(req, res) => {
+const validateAlbum = [
+  check('title')
+    .notEmpty()
+    .withMessage('Please provide a title')
+    .isLength({ min: 1, max: 200 })
+    .withMessage('Please provide a title between 1-200 characters')
+];
+
+router.post('/newAlbum', validateAlbum, requireAuth, asyncHandler(async(req, res) => {
   const { title } = req.body;
-  const newAlbum = await Album.create({
-    userId: req.user.id,
-    title,
-  })
-  return res.json({ newAlbum })
+  const validationErrors = validationResult(req);
+
+  if (validationErrors.isEmpty()) {
+    const newAlbum = await Album.create({
+      userId: req.user.id,
+      title,
+    })
+    return res.json({ newAlbum })
+  } else {
+    const errors = validationErrors.Array().map((err) => err.msg);
+    return res.json(errors);
+  }
+
+}))
+
+router.put('/:albumId(\\d+)/:albumName/edit', validateAlbum, requireAuth, asyncHandler(async(req, res) => {
+  const { title } = req.body;
+  const { albumId } = req.params;
+  const album = await Album.findByPk(albumId);
+
+  const validationErrors = validateAlbum(req);
+
+  if (validationErrors.isEmpty()) {
+    await album.update({
+      title
+    })
+  
+
+  const updatedAlbum = await Album.findByPk(albumId);
+  return res.json({updatedAlbum});
+  } else {
+    const errors = validationErrors.Array().map((err) => err.msg);
+    return res.json(errors);
+  }
+
+
 }))
 
 module.exports = router;
