0a4d5f6755e0dbdc936abafe867f2a39fcfd0018
Chocoloco123 <csarkki@gmail.com>
2022-01-26 21:35:15 -0800

    able to render photos for the album page

--- frontend/src/components/AlbumImagesPage/index.js
+++ frontend/src/components/AlbumImagesPage/index.js
@@ -3,8 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useParams, useHistory, Redirect } from 'react-router-dom';
 import { deleteAnAlbum, getAlbums } from '../../store/albums';
-
-// import { getAlbums } from '../../store/albums';
+import { getImages } from '../../store/images';
 
 import './AlbumImagePage.css'
 
@@ -12,20 +11,30 @@ const AlbumImagesPage = () => {
   const history = useHistory();
   const params = useParams();
   const dispatch = useDispatch();
-  const imagesObj = useSelector((state) => state.images)
-  const albumsObj = useSelector((state) => state.albums);
-  const sessionUser = useSelector((state) => state.session.user);
   const { albumName, albumId } = params;
+  const imagesObj = useSelector((state) => state.images);
+  const imagesArr = Object.values(imagesObj);
+  
+  const albumImagesArr = imagesArr.filter((obj) => (
+    obj?.albumId === +albumId
+  ));
+  console.log(albumImagesArr)
+
+  const albumsObj = useSelector((state) => state.albums);
+  
+  const sessionUser = useSelector((state) => state.session.user);
   const albumTitle = albumsObj[albumId]?.title;
   const theAlbumId = albumsObj[albumId];
   // console.log('theAlbumId: ',theAlbumId)
   
-  const imagesArr = Object.values(imagesObj);
 
   const handleDelete = async(albumId) => {
     await dispatch(deleteAnAlbum(albumId));
     history.push('/albums')
   }
+  useEffect(() => {
+    dispatch(getImages())
+  }, [dispatch])
 
   useEffect(() => {
     dispatch(getAlbums())
@@ -45,7 +54,17 @@ const AlbumImagesPage = () => {
         <button onClick={() => handleDelete(theAlbumId?.id)}>Delete</button>
       </div>
       <div>
-        {imagesArr.length < 0 ? 'hello world' : 'empty' }
+        {albumImagesArr.map((obj) => 
+          <div key={"albumImgDiv"+obj.id}>
+            <NavLink to={`/images/${obj?.id}`} key={obj.id}>
+              <img 
+            src={obj.imageUrl} 
+            alt={obj.imageTitle} 
+            className='homeImages'>
+          </img>
+            </NavLink>
+          </div>
+        )}
       </div>
     </div>  
   )
