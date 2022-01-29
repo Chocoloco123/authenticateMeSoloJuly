f9436edccf94a4a45dc082caa0dcdd84ff719335
Chocoloco123 <csarkki@gmail.com>
2022-01-29 09:16:53 -0800

    commented out model edits

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       references: { model: 'Albums' },
       allowNull: true,
-      onDelete: "SET NULL",
-      hooks: true
+      // onDelete: "SET NULL",
+      // hooks: true
     },
     imageUrl: {
       type: DataTypes.STRING,
@@ -28,8 +28,8 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
     Image.belongsTo(models.User, { foreignKey: 'userId' }),
     // Image.belongsTo(models.Album, { foreignKey: 'albumId', onDelete: "SET NULL", hooks: true }),
-    // Image.belongsTo(models.Album, { foreignKey: 'albumId' }),
-    Image.belongsTo(models.Album, { foreignKey: { name: 'albumId', allowNull: true}, onDelete: "SET NULL", hooks: true }),
+    Image.belongsTo(models.Album, { foreignKey: 'albumId' }),
+    // Image.belongsTo(models.Album, { foreignKey: { name: 'albumId', allowNull: true}, onDelete: "SET NULL", hooks: true }),
     Image.hasMany(models.Comment, { foreignKey: 'imageId', onDelete: 'CASCADE', hooks: true  })
   };
   return Image;
